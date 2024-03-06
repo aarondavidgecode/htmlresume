943ed6cc959d1cc4fe4cf6f6121d3046ff117c69
Aaron Davidge <120982492+aarondavidgecode@users.noreply.github.com>
2024-03-06 22:23:05 +0000

    basic html information

---
+++ assets/images/javascript/index.js
@@ -0,0 +1,8 @@
+function updateProgress(percent) {
+    document.getElementById('progress').style.width = percent + '%';
+  }
+  
+  // Example usage:
+  // Call updateProgress(percent) with the percentage of progress
+  updateProgress(80);
+  
